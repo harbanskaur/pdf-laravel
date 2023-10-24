f0ce4c20e8e0edd02f9b7ff720f8cb56009ea75f
harbanskaur <harbanskaur1602@gmail.com>
2023-10-24 15:13:03 +0530

    files added

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-use TCPDF;
+use TCPDF;//inbuild class 
 use Illuminate\Http\Request;
 use App\Models\Student;
 class PdfController extends Controller
-{
+{   //view function
     public function index(){
         $student=Student::all();
         return view('pdf',compact('student'));
@@ -13,25 +13,24 @@ class PdfController extends Controller
 
     public function generatePDF(Request $request){
         $data=Student::all();
-        $pdf=new TCPDF();
-        $pdf->AddPage();
-        $pdf->SetFont('dejavusans', '', 12);//optional
+        $pdf=new TCPDF();//object
+        $pdf->AddPage();//inbuild method
+        $pdf->SetFont('dejavusans', '', 10);//optional
         // Add column headings
-        $pdf->Cell(30, 10, 'Id', 1);
-        $pdf->Cell(30, 10, 'First Name', 1);
-        $pdf->Cell(30, 10, 'Last Name', 1);
-        $pdf->Cell(30, 10, 'Marks1', 1);
-        $pdf->Cell(30, 10, 'Marks2', 1);
-        $pdf->Ln(); 
+        $pdf->Cell(30, 10, 'Id', 1);        
+        $pdf->Cell(30, 10, 'Name', 1);
+        $pdf->Cell(30, 10, 'Gender', 1);
+        $pdf->Cell(50, 10, 'Department', 1);
+        $pdf->Ln(); // next line inbuild function
 
         foreach ($data as $row) {
             $pdf->Cell(30, 10, $row->id, 1);
-            $pdf->Cell(30, 10, $row->first_name, 1);
-            $pdf->Cell(30, 10, $row->last_name, 1);
-            $pdf->Cell(30, 10, $row->marks1, 1);
-            $pdf->Cell(30, 10, $row->marks2, 1);
+            $pdf->Cell(30, 10, $row->name, 1);
+            $pdf->Cell(30, 10, $row->gender, 1);
+            $pdf->Cell(50, 10, $row->department, 1);
             $pdf->Ln(); 
         }
-        return $pdf->Output('student.pdf','I');
+         return $pdf->Output('employee.pdf','I');
+        // return $pdf->Output('employee.pdf','D');//inbuild function
     }
 }
